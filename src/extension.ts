7ce4904826b9c988b73d0795abea4cd6c976ed05
Oliver Calderón <loviver.calderon@gmail.com>
2025-03-16 23:59:58 -0600

    🎨 Refactor to enhance clarity of commit confirmation message in terminal

--- src/extension.ts
+++ src/extension.ts
@@ -60,7 +60,7 @@ async function requestCommitSuggestions(
           const terminal = vscode.window.activeTerminal || vscode.window.createTerminal("Mi Terminal");
           
           if(terminal) {
-            terminal.sendText(`echo "Commit confirmado ${commit.commit}: ${selectedMessage}"`);
+            terminal.sendText(`✅ Commit confirmado: ${commit.commit} | 📜 ${selectedMessage}`);
           }
 
           vscode.window.showInformationMessage(i18n.t('success.commit.confirmed', { error: selectedMessage }));
